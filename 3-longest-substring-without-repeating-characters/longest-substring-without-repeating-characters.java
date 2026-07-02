21f37732139843cc7dbcadedd1c821f5fc5415d3
Poornakala2006 <pp3869276@gmail.com>
2026-07-02 10:54:24 +0530

    Time: 7 ms (36.90%) | Memory: 46.5 MB (38.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,21 +3,19 @@ class Solution {
 
         int left = 0, right=0 , sLen = s.length();
         int maxLength = 0;
-         HashMap<Character,Integer> h1 = new HashMap<>();
+        HashMap<Character,Integer> h1 = new HashMap<>();
         while(right < sLen)
         {
-           
-            while(left < right && h1.containsKey(s.charAt(right))==true)
+           while(left < right && h1.containsKey(s.charAt(right))==true)
             {
                 h1.remove(s.charAt(left));
-
-                left++;
+                 left++;
             }
             maxLength = Math.max(maxLength , right-left+1);
             h1.put(s.charAt(right), 1);
             right++;
-         }
-         return  maxLength;
+        }
+        return  maxLength;
          
     }
 }
